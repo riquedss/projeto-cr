1dc6a8956af44403753f7d333e1de66e46a60f8e
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-30 23:57:48 -0300

    feat(main): add view

---
+++ app/view/main_view.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def message_main
+  print " ________________________________________________________________________\n"
+  print "|                                                                        |\n"
+  print "|                                                                        |\n"
+  print "|            ______________________________________________              |\n"
+  print "|           |                                              |             |\n"
+  print "|           |     Seja Bem-vindo ao sistema de Notas       |             |\n"
+  print "|           |      Desenvolvido por Henrique Santana       |             |\n"
+  print "|           |                 Version: 1.0                 |             |\n"
+  print "|           |                                              |             |\n"
+  print "|           |______________________________________________|             |\n"
+  print "|                                                                        |\n"
+  print "|________________________________________________________________________|\n"
+end
